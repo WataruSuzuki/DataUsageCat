e3ed88fa5fb47bdb9463f5056afe0c0c18db979c
WataruSuzuki <wataru0406@gmail.com>
2019-09-11 22:20:00 +0900

    Set up github actions

---
+++ .github/workflows/xcodebuild.sh
@@ -0,0 +1,26 @@
+VERSION="latest"
+
+if [ $# -eq 1 ]; then
+    VERSION="$1"
+fi
+
+function exe_fast_lane() {
+    fastlane "tests_$VERSION"
+}
+
+# CocoaPods
+  # run: |
+pod install --repo-update
+
+# Change xcode version
+  # run: |
+sudo xcode-select -s '/Applications/Xcode_11.3.app/Contents/Developer'
+
+# Check environment
+  # run: |
+xcodebuild -version
+instruments -s device
+
+# Run XCTest and XCUITest
+  # run: |
+exe_fast_lane

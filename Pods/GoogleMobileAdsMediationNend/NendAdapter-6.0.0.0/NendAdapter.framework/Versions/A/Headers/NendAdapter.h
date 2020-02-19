d2035e7023f20c893a455f6e7814df5bb5e65599
WataruSuzuki <wataru0406@gmail.com>
2020-02-19 22:30:00 +0900

    Updated dependencies : pod update

---
+++ Pods/GoogleMobileAdsMediationNend/NendAdapter-6.0.0.0/NendAdapter.framework/Versions/A/Headers/NendAdapter.h
@@ -0,0 +1,11 @@
+//
+//  NendAdapter.h
+//  NendAdapter
+//
+//  Copyright © 2017 FAN Communications. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+#import <NendAdapter/GADMAdapterNendExtras.h>
